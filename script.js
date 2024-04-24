c2b2ba7273d415f943847e9ded993531554e98dc
İnan Derin Akın <inandakin@gmail.com>
2024-04-24 14:26:58 +0300

    23/04/2024 changes

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+var btnNotification = document.getElementById('btn-notification');
+var btnStart = document.getElementById('btn-start');
+var btnSetting = document.getElementById('btn-setting');
+var notification = document.getElementById('notification');
+var start = document.getElementById('start');
+var setting = document.getElementById('setting');
+
+btnNotification.addEventListener('click', function() {
+    start.style.display = 'none';
+    setting.style.display = 'none';
+    notification.style.display = 'flex';
+});
+
+btnStart.addEventListener('click', function() {
+    start.style.display = 'flex';
+    setting.style.display = 'none';
+    notification.style.display = 'none';
+});
+
+btnSetting.addEventListener('click', function() {
+    start.style.display = 'none';
+    setting.style.display = 'flex';
+    notification.style.display = 'none';
+});
+
 btnnotification.getElementById('btn-notification').addEventListener('click', function() {
     start.getElementById('start').style.display = 'none';
     setting.getElementById('setting').style.display = 'none';
